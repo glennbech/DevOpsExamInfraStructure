b3ad810fd73cbc857da6ad355a62b70a1f66fe59
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-11-19 20:56:40 +0100

    fix

--- provider.tf
+++ provider.tf
@@ -2,13 +2,13 @@ terraform {
   backend "gcs" {
     bucket = "maniac"
     prefix = "terraformstate"
-    credentials = "${file("terraform.json")}"
+    credentials = "terraform.json"
   }
 }
 
 
 provider "google-beta" {
-  credentials = "${file("terraform.json")}"
+  credentials = "terraform.json"
   project = var.project_id
   version = "~> 3.0.0-beta.1"
 }
